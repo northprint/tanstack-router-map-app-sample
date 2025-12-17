8c1f335f7ab3cc0a4d8b71eef7c098bcc59f7d21
Norihiro Narayama <45268313+northprint@users.noreply.github.com>
2025-12-17 11:07:47 +0900

    デバッグツールは消す

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,9 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      {/* ページの中身がここに描画される */}
       <Outlet />
-      {/* デバッグ用のツール（本番では消える） */}
-      <TanStackRouterDevtools />
     </>
   ),
 })
